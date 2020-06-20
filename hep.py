745d67b74f92d35dd681d419e668105d0a664ec1
Matt Groth <mgroth49@gmail.com>
2020-06-20 11:10:01 -0400

    cleanup main script

--- hep.py
+++ hep.py
@@ -7,7 +7,6 @@ from HEP_Params import SUBJECTS, PLOT, SAVE
 
 mains = tuple([s.plots() for s in SUBJECTS])
 mains = np.vstack(mains)
-figs = mains
 ar = np.ndarray(shape=(1, 1), dtype=MultiPlot)
 comp = [[compare_IBI(SUBJECTS[0], SUBJECTS[1])]]
 ar[:, :] = comp
@@ -17,5 +16,5 @@ figs = np.vstack(figs)
 makefig(figs, plotarg=PLOT)
 if SAVE:
     new_data = any([s.savepeaks() for s in SUBJECTS])
-else: new_data=False
+else: new_data = False
 log(f'Finished heartbeat analysis!({new_data=})')
