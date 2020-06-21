8b42b7a1098fb3cf07c3d8d82b97255eff3e7f20
Matt Groth <mgroth49@gmail.com>
2020-06-20 21:25:22 -0400

    multi page website test

--- hep.py
+++ hep.py
@@ -1,22 +1,19 @@
-import time
-
 from mlib.FigData import makefig
 from HEP_lib import compare_IBI
 from HEP_Params import SUBJECTS, PLOT_FILE, SAVE
-from mlib.boot.mutil import refreshSafariReport, shell, File
-from mlib.web.makereport_lib import prep_webpage, push_docs
-from mlib.web.web import HTML, HTMLImage, AutoHTMLImage
+from mlib.web.makereport_lib import DOCS_FOLDER
+from mlib.web.web import HTMLPage, AutoHTMLImage, HTMLIndex
 figs = [[plot] for s in SUBJECTS for plot in s.plots()]
 figs += [[compare_IBI(SUBJECTS[0], SUBJECTS[1])]]
 makefig(figs, file=PLOT_FILE, show=False)
-DOCS_ROOT = 'https://mgroth0.github.io/hep/'
-DOCS_ROOT_OFFLINE_VERSION = File('_docs/index.html').url()
-IMAGE_ROOT = 'https://media.githubusercontent.com/media/mgroth0/hep/master/docs/'
-prep_webpage(
-    HTML(
-        AutoHTMLImage(f'resources/figs/plot.png'),
+HTMLIndex(
+    HTMLPage(
+        'main',
+        AutoHTMLImage(PLOT_FILE.rel_to(DOCS_FOLDER)),
     ),
-    web_resources_root=IMAGE_ROOT
-)
-refreshSafariReport(DOCS_ROOT_OFFLINE_VERSION)
+    HTMLPage(
+        'test1/test2',
+        AutoHTMLImage(PLOT_FILE.rel_to(DOCS_FOLDER)),
+    )
+).write().open()
 if SAVE: [s.savepeaks() for s in SUBJECTS]
